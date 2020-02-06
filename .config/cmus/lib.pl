4d9a52e2ac3d870b0f4ad6818409cdf25cc22765
Erekle Tvinadze <erekle.tvinadze.1@btu.edu.ge>
2020-02-06 15:24:38 +0400

    added cmus

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,55 @@
+/home/unknown/music/The Immortal Wars [2017]/01 Ex Deo - The Immortal Wars [2017] - The Rise of Hannibal.flac
+/home/unknown/music/The Immortal Wars [2017]/02 Ex Deo - The Immortal Wars [2017] - Hispania (The Siege of Saguntum).flac
+/home/unknown/music/The Immortal Wars [2017]/03 Ex Deo - The Immortal Wars [2017] - Crossing of the Alps.flac
+/home/unknown/music/The Immortal Wars [2017]/04 Ex Deo - The Immortal Wars [2017] - Suavetaurilia (Intermezzo).flac
+/home/unknown/music/The Immortal Wars [2017]/05 Ex Deo - The Immortal Wars [2017] - Cato Major_ Carthago delenda est!.flac
+/home/unknown/music/The Immortal Wars [2017]/06 Ex Deo - The Immortal Wars [2017] - Ad Victoriam (The Battle of Zama).flac
+/home/unknown/music/The Immortal Wars [2017]/07 Ex Deo - The Immortal Wars [2017] - The Spoils of War.flac
+/home/unknown/music/The Immortal Wars [2017]/08 Ex Deo - The Immortal Wars [2017] - The Roman.flac
+/home/unknown/music/The Elysian Grandeval Gal?riarch (Album)/01 Unholy Gravebirth.flac
+/home/unknown/music/The Elysian Grandeval Gal?riarch (Album)/02 Crucifilth.flac
+/home/unknown/music/The Elysian Grandeval Gal?riarch (Album)/03 Motherless Miscarriage.flac
+/home/unknown/music/The Elysian Grandeval Gal?riarch (Album)/04 Baptised Bastardised Sodomised.flac
+/home/unknown/music/The Elysian Grandeval Gal?riarch (Album)/05 Behold the Kingdom of the Wretched Undying (feat. Tyler Shelton Bryan Long Dillon Becker & Chris Whited).flac
+/home/unknown/music/The Elysian Grandeval Gal?riarch (Album)/06 Soil the Stillborn.flac
+/home/unknown/music/The Elysian Grandeval Gal?riarch (Album)/07 Paedophilic Ultimatum.flac
+/home/unknown/music/The Elysian Grandeval Gal?riarch (Album)/08 Neutered in Utero.flac
+/home/unknown/music/The Elysian Grandeval Gal?riarch (Album)/09 Pelt of Innocent Flesh.flac
+/home/unknown/music/The Elysian Grandeval Gal?riarch (Album)/10 Blasphemian.flac
+/home/unknown/music/The Elysian Grandeval Gal?riarch (Album)/11 Neonatalimpalionecrophiliation.flac
+/home/unknown/music/Infant Annihilator - The Battle of Yaldabaoth 2019/01. Childchewer.flac
+/home/unknown/music/Infant Annihilator - The Battle of Yaldabaoth 2019/02. Three Bastards.flac
+/home/unknown/music/Infant Annihilator - The Battle of Yaldabaoth 2019/03. The Kingdom Sitteth Lonely Beneath Thine Hollowed Heavens (feat. Trevor Strnad).flac
+/home/unknown/music/Infant Annihilator - The Battle of Yaldabaoth 2019/04. Ov Sacrament and Sincest (feat. Storm Strope).flac
+/home/unknown/music/Infant Annihilator - The Battle of Yaldabaoth 2019/05. Feast ov Goreglutton.flac
+/home/unknown/music/Infant Annihilator - The Battle of Yaldabaoth 2019/06. Plaguebearer.flac
+/home/unknown/music/Infant Annihilator - The Battle of Yaldabaoth 2019/07. Swinaecologist.flac
+/home/unknown/music/Infant Annihilator - The Battle of Yaldabaoth 2019/08. A Rape of Sirens (feat. Alex Terrible).flac
+/home/unknown/music/Infant Annihilator - The Battle of Yaldabaoth 2019/09. Empusa- Queen of the Damned.flac
+/home/unknown/music/Infant Annihilator - The Battle of Yaldabaoth 2019/10. Ere the Crimson Dawn.flac
+/home/unknown/music/Infant Annihilator - The Battle of Yaldabaoth 2019/11. Thy Faith, Thy Oblivion.flac
+/home/unknown/music/Infant Annihilator - The Battle of Yaldabaoth 2019/12. The Battle of Yaldabaoth (feat. Alex Teyen).flac
+/home/unknown/music/Infant Annihilator - The Battle of Yaldabaoth 2019/13. Necropocalypse.flac
+/home/unknown/music/Infant Annihilator - The Battle of Yaldabaoth 2019/14. Paedophilic Legacy.flac
+/home/unknown/music/Lorna Shore - Psalms - 2015/01 - Grimoire.flac
+/home/unknown/music/Lorna Shore - Psalms - 2015/02 - Harvest Realms.flac
+/home/unknown/music/Lorna Shore - Psalms - 2015/03 - Throne of Worms.flac
+/home/unknown/music/Lorna Shore - Psalms - 2015/04 - White Noise.flac
+/home/unknown/music/Lorna Shore - Psalms - 2015/05 - From the Pale Mist.flac
+/home/unknown/music/Lorna Shore - Psalms - 2015/06 - Infernal Haunting.flac
+/home/unknown/music/Lorna Shore - Psalms - 2015/07 - Death Gowns.flac
+/home/unknown/music/Lorna Shore - Psalms - 2015/08 - Wretching In Torment.flac
+/home/unknown/music/Lorna Shore - Psalms - 2015/09 - Traces of Supremacy.flac
+/home/unknown/music/Lorna Shore - Psalms - 2015/10 - Eternally Oblivion.flac
+/home/unknown/music/Lorna Shore - Flesh Coffin - 2017/1 - Offering of Fire.flac
+/home/unknown/music/Lorna Shore - Flesh Coffin - 2017/2 - Denounce the Light.flac
+/home/unknown/music/Lorna Shore - Flesh Coffin - 2017/3 - The Astral Wake of Time.flac
+/home/unknown/music/Lorna Shore - Flesh Coffin - 2017/4 - Desolate Veil.flac
+/home/unknown/music/Lorna Shore - Flesh Coffin - 2017/5 - FVNERAL MOON.flac
+/home/unknown/music/Lorna Shore - Flesh Coffin - 2017/6 - Void.flac
+/home/unknown/music/Lorna Shore - Flesh Coffin - 2017/7 - Infernum.flac
+/home/unknown/music/Lorna Shore - Flesh Coffin - 2017/8 - The__Watcher.flac
+/home/unknown/music/Lorna Shore - Flesh Coffin - 2017/9 - Black Hollow.flac
+/home/unknown/music/Lorna Shore - Flesh Coffin - 2017/10 - Flesh Coffin.flac
+/home/unknown/music/Lorna Shore - This is Hell - 2018/1 - This is Hell.flac
+/home/unknown/music/Lorna Shore - Darkest Spawn (2019)/01. Darkest Spawn.flac
